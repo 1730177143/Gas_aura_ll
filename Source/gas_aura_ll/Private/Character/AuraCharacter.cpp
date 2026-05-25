9b6bbc18b51c3996e6d4afa28964d4405b7c9519
long liu <1730177143@qq.com>
2026-05-25 17:28:06 +0800

    项目创建-12-enemyinterface

--- Source/gas_aura_ll/Private/Character/AuraCharacter.cpp
+++ Source/gas_aura_ll/Private/Character/AuraCharacter.cpp
@@ -7,10 +7,15 @@
 
 AAuraCharacter::AAuraCharacter()
 {
+	// 让角色的旋转自动朝向移动方向，
 	GetCharacterMovement()->bOrientRotationToMovement = true;
+	// 设置旋转速率：仅在 Yaw 轴（水平旋转）上以 400 度/秒的速度平滑转向
 	GetCharacterMovement()->RotationRate = FRotator(0.0f, 400.0f, 0.0f);
+	// 将移动约束在一个平面内（通常为地面），避免产生垂直位移
 	GetCharacterMovement()->bConstrainToPlane = true;
 
+	// 以下三项全部设为 false，表示角色的朝向不再跟随控制器的 Pitch / Yaw / Roll
+	// 因为我们希望角色旋转完全由移动方向驱动，而不是由视角方向决定
 	bUseControllerRotationPitch = false;
 	bUseControllerRotationYaw = false;
 	bUseControllerRotationRoll = false;
