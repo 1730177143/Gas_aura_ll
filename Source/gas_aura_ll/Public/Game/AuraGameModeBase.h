342d15581adea6c0ca1d8155544d7e092405d462
long liu <1730177143@qq.com>
2026-05-25 17:16:47 +0800

    项目创建-11-gamemode+修复移动输入

---
+++ Source/gas_aura_ll/Public/Game/AuraGameModeBase.h
@@ -0,0 +1,17 @@
+// gas_aura_ll
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "AuraGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class GAS_AURA_LL_API AAuraGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
