da37a08c6e77754c73b824a7108768f06b627336
long liu <1730177143@qq.com>
2026-05-28 12:00:59 +0800

    gas简介-3-playerstate

---
+++ Source/gas_aura_ll/Private/Player/AuraPlayerState.cpp
@@ -0,0 +1,9 @@
+// gas_aura_ll
+
+
+#include "Player/AuraPlayerState.h"
+
+AAuraPlayerState::AAuraPlayerState()
+{
+	NetUpdateFrequency = 100.f;
+}
---
+++ Source/gas_aura_ll/Public/Player/AuraPlayerState.h
@@ -0,0 +1,19 @@
+// gas_aura_ll
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/PlayerState.h"
+#include "AuraPlayerState.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class GAS_AURA_LL_API AAuraPlayerState : public APlayerState
+{
+	GENERATED_BODY()
+	
+	public:
+	AAuraPlayerState();
+};
