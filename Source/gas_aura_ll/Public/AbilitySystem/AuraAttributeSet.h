0fa7c8e2fc141e5962bf69e7a0d58453375eff91
long liu <1730177143@qq.com>
2026-05-30 16:22:22 +0800

    gas简介-4-生命值和法力等属性的样板代码设置

--- Source/gas_aura_ll/Public/AbilitySystem/AuraAttributeSet.h
+++ Source/gas_aura_ll/Public/AbilitySystem/AuraAttributeSet.h
@@ -13,5 +13,26 @@ UCLASS()
 class GAS_AURA_LL_API UAuraAttributeSet : public UAttributeSet
 {
 	GENERATED_BODY()
-	
+
+public:
+	UAuraAttributeSet();
+	virtual void GetLifetimeReplicatedProps(TArray<FLifetimeProperty>& OutLifetimeProps) const override;
+
+	UPROPERTY(BlueprintReadOnly, ReplicatedUsing=OnRep_Health, Category="基本属性")
+	FGameplayAttributeData Health;
+	UPROPERTY(BlueprintReadOnly, ReplicatedUsing=OnRep_MaxHealth, Category="基本属性")
+	FGameplayAttributeData MaxHealth;
+	UPROPERTY(BlueprintReadOnly, ReplicatedUsing=OnRep_Mana, Category="基本属性")
+	FGameplayAttributeData Mana;
+	UPROPERTY(BlueprintReadOnly, ReplicatedUsing=OnRep_MaxMana, Category="基本属性")
+	FGameplayAttributeData MaxMana;
+
+	UFUNCTION()
+	void OnRep_Health(const FGameplayAttributeData& OldHealth) const;
+	UFUNCTION()
+	void OnRep_MaxHealth(const FGameplayAttributeData& OldMaxHealth) const;
+	UFUNCTION()
+	void OnRep_Mana(const FGameplayAttributeData& OldMana) const;
+	UFUNCTION()
+	void OnRep_MaxMana(const FGameplayAttributeData& OldMaxMana) const;
 };
