3cd2468b1e916babc64d87dc55011314bc08a9d5
long liu <1730177143@qq.com>
2026-06-02 17:42:38 +0800

    GameplayTags-6-Gameplay Effect委托绑定函数

--- Source/gas_aura_ll/Private/Character/AuraEnemy.cpp
+++ Source/gas_aura_ll/Private/Character/AuraEnemy.cpp
@@ -10,12 +10,14 @@
 
 AAuraEnemy::AAuraEnemy()
 {
+	// 让敌人的网格体对ECC_Visibility产生阻挡响应，常用于高亮描边或目标选择
 	GetMesh()->SetCollisionResponseToChannel(ECC_Visibility, ECR_Block);
-
+	// 创建自定义的 ASC 并设置为网络可复制
 	AbilitySystemComponent = CreateDefaultSubobject<UAuraAbilitySystemComponent>("AbilitySystemComponent");
 	AbilitySystemComponent->SetIsReplicated(true);
+	// 使用 Minimal 复制模式，仅复制Gameplay Cues和Gameplay Tags,Gameplay Effects不会被复制
 	AbilitySystemComponent->SetReplicationMode(EGameplayEffectReplicationMode::Minimal);
-
+	// 创建属性集
 	AttributeSet = CreateDefaultSubobject<UAuraAttributeSet>("AuraAttributeSet");
 }
 
@@ -37,5 +39,14 @@ void AAuraEnemy::BeginPlay()
 {
 	Super::BeginPlay();
 
+	InitAbilityActorInfo();
+}
+
+void AAuraEnemy::InitAbilityActorInfo()
+{
+	// 将自身注册为 ASC 的owner和Avatar，这是 ASC 正常工作的前提
 	AbilitySystemComponent->InitAbilityActorInfo(this, this);
+	// 通知自定义 ASC：ActorInfo 已设置完毕，可以绑定内部委托
+	// 这样做使 Character 只需要单向依赖 ASC，ASC 自身负责处理效果事件
+	Cast<UAuraAbilitySystemComponent>(AbilitySystemComponent)->AbilityActorInfoSet();
 }
--- Source/gas_aura_ll/Public/AbilitySystem/AuraAbilitySystemComponent.h
+++ Source/gas_aura_ll/Public/AbilitySystem/AuraAbilitySystemComponent.h
@@ -13,5 +13,10 @@ UCLASS()
 class GAS_AURA_LL_API UAuraAbilitySystemComponent : public UAbilitySystemComponent
 {
 	GENERATED_BODY()
-	
+public:
+	// 当 AbilityActorInfo 设置完成后调用，用于绑定效果应用的委托
+	void AbilityActorInfoSet();
+protected:
+	void EffectApplied(UAbilitySystemComponent* AbilitySystemComponent, const FGameplayEffectSpec& EffectSpec,
+	                   FActiveGameplayEffectHandle ActiveEffectHandle);
 };
