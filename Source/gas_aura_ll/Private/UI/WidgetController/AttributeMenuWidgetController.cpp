7a407a04d4ebae956776042ef41afcf6b2a69626
long liu <1730177143@qq.com>
2026-06-06 17:53:16 +0800

    属性菜单UI-21-BindCallBackToDependencies()变动时触发委托

--- Source/gas_aura_ll/Private/UI/WidgetController/AttributeMenuWidgetController.cpp
+++ Source/gas_aura_ll/Private/UI/WidgetController/AttributeMenuWidgetController.cpp
@@ -12,7 +12,6 @@ void UAttributeMenuWidgetController::BroadcastInitialValues()
 	check(AttributeInfo);
 	for (auto& Pair : AS->TagsToAttributes)
 	{
-		
 		// 加上() 调用静态函数 Pair.Value()，得到 FGameplayAttribute
 		BroadcastAttributeInfo(Pair.Key, Pair.Value());
 	}
@@ -20,6 +19,20 @@ void UAttributeMenuWidgetController::BroadcastInitialValues()
 
 void UAttributeMenuWidgetController::BindCallBackToDependencies()
 {
+	UAuraAttributeSet* AS = CastChecked<UAuraAttributeSet>(AttributeSet);
+	check(AttributeInfo);
+	for (auto& Pair : AS->TagsToAttributes)
+	{
+		AbilitySystemComponent->GetGameplayAttributeValueChangeDelegate(Pair.Value()).AddLambda(
+			//委托签名强制要求 const FOnAttributeChangeData& Data
+			[this,Pair](const FOnAttributeChangeData& Data)
+			{
+				BroadcastAttributeInfo(Pair.Key, Pair.Value());
+			}
+
+		);
+		// 加上() 调用静态函数 Pair.Value()，得到 FGameplayAttribute
+	}
 }
 
 void UAttributeMenuWidgetController::BroadcastAttributeInfo(const FGameplayTag& AttributeTag,
