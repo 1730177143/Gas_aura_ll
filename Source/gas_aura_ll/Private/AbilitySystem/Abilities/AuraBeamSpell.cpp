d7496d1347fa60e107a3cd8dc480cd0adf66cb43
long liu <1730177143@qq.com>
2026-06-23 01:24:18 +0800

    高级GA-6-AuraBeamSpell

---
+++ Source/gas_aura_ll/Private/AbilitySystem/Abilities/AuraBeamSpell.cpp
@@ -0,0 +1,29 @@
+// gas_aura_ll
+
+
+#include "AbilitySystem/Abilities/AuraBeamSpell.h"
+
+#include "GameFramework/Character.h"
+#include "Kismet/KismetSystemLibrary.h"
+
+void UAuraBeamSpell::StoreMouseDataInfo(const FHitResult& HitResult)
+{
+	if (HitResult.bBlockingHit)
+	{
+		MouseHitLocation = HitResult.ImpactPoint;
+		MouseHitActor = HitResult.GetActor();
+	}
+	else
+	{
+		CancelAbility(CurrentSpecHandle, CurrentActorInfo, CurrentActivationInfo, true);
+	}
+}
+
+void UAuraBeamSpell::StoreOwnerVariables()
+{
+	if (CurrentActorInfo)
+	{
+		OwnerPlayerController = CurrentActorInfo->PlayerController.Get();
+		OwnerCharacter = Cast<ACharacter>(CurrentActorInfo->AvatarActor);
+	}
+}
---
+++ Source/gas_aura_ll/Public/AbilitySystem/Abilities/AuraBeamSpell.h
@@ -0,0 +1,38 @@
+// gas_aura_ll
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "AbilitySystem/Abilities/AuraDamageGameplayAbility.h"
+#include "AuraBeamSpell.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class GAS_AURA_LL_API UAuraBeamSpell : public UAuraDamageGameplayAbility
+{
+	GENERATED_BODY()
+
+public:
+	UFUNCTION(BlueprintCallable)
+	void StoreMouseDataInfo(const FHitResult& HitResult);
+
+	UFUNCTION(BlueprintCallable)
+	void StoreOwnerVariables();
+
+
+protected:
+	UPROPERTY(BlueprintReadWrite, Category = "Beam")
+	FVector MouseHitLocation;
+
+	UPROPERTY(BlueprintReadWrite, Category = "Beam")
+	TObjectPtr<AActor> MouseHitActor;
+
+	UPROPERTY(BlueprintReadWrite, Category = "Beam")
+	TObjectPtr<APlayerController> OwnerPlayerController;
+
+	UPROPERTY(BlueprintReadWrite, Category = "Beam")
+	TObjectPtr<ACharacter> OwnerCharacter;
+	
+};
